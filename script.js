4425500ea36dee205d76c40d0dcfdcc5441b9c74
amin10003 <maxamedxasancali10003@gmail.com>
2026-04-06 12:05:07 +0300

    created  index.html with full styled form and empty script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Starting My project...")
+
+
+
+function saveUseData(){
+    const nameInput = document.getElementById("name")
+    
+const birthInput = document.getElementById("DoB")
+const submitBtn = document.querySelector("button")
+}
